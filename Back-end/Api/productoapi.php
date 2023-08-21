e87bab09292bcd1c766f8f1f3c2ece797adc2b55
Josseline16 <joceastu@espol.edu.ec>
2023-08-20 22:38:48 -0500

    api

---
+++ Back-end/Api/productoapi.php
@@ -0,0 +1,84 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+//echo "metodo http: ".$_SERVER['REQUEST_METHOD'];
+//echo 'Informacion:' . file_get_contents('php://input'); //lee lo que mando el cliente
+//indicar que estamos mandando un json
+header('Content-Type: application/json; charset=utf-8'); //indico que le estoy mandando un json
+include_once("../Clases/producto.php");
+
+switch($_SERVER['REQUEST_METHOD']){
+    //agregar productos
+    case 'POST':
+        $_POST = json_decode(file_get_contents('php://input'),true); //obtiene en json format
+
+        $id_prod = $_POST['id'];
+        
+        $nombre_prod = $_POST['nombre'];
+        $precio_prod = $_POST['precio'];
+        $stock = $_POST['stock'];
+        $imageURL = $_POST['imageURL'];
+        $descr_prod = $_POST['descripcion'];
+        $categoria = $_POST['categoria'];
+
+        $producto = new Producto($id_prod, $nombre_prod, $precio_prod, $stock, $imageURL,  $descr_prod,  $categoria);
+        $producto->addProducts();
+
+        break;
+
+    //obtener productos
+    case 'GET':
+        if (isset($_GET['id'])){
+            //mostrar productos por id
+            Producto::getProducto($_GET['id']);
+
+        }else if (isset($_GET['categoria'])){
+
+            //mostrar productos por categoría
+             Producto::showByCategories($_GET['categoria']);
+        }else if (isset($_GET['greater'])){
+            //muestra los productos que tengan un precio mayor a un precio
+            Producto::showProductsGreaterThan($_GET['greater']);
+        }else if  (isset($_GET['less'])){
+
+            //muestra los productos que tengan un precio menor a un precio
+            Producto::showProductsLessThan($_GET['less']);
+        }else{
+            //obtener todos los productos
+            Producto::getProductos();
+        }
+
+        break;
+    
+    //actualizar productos
+    case 'PUT':
+        if (isset($_GET['id'])) {
+            $_PUT = json_decode(file_get_contents('php://input'),true);
+            $producto = new Producto($_PUT['id'], $_PUT['startUp'],$_PUT['nombre'],$_PUT['precio'],$_PUT['stock'],$_PUT['imageURL'],$_PUT['descripcion'],$_PUT['categoria']);
+            $producto->updateProduct($_GET['id']);
+        }
+        break;
+
+    //eliminar productos
+    case 'DELETE':
+        if(isset($_GET['id'])){
+            Producto::deleteProduct($_GET['id']);
+        }
+        break;
+}
+
+
+
+?> 
